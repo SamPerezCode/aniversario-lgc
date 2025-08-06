30556bfdce9f080ea32b24527428b16e5f227e13
Samyr Perez <samperezcode.2024@gmail.com>
2025-08-06 06:05:52 -0500

    Refactor calcularMonto para eliminar lógica de preventa y ajustar valor por modalidad

--- src/utils/calcularMonto.js
+++ src/utils/calcularMonto.js
@@ -8,22 +8,34 @@ const obtenerFechaBogota = () => {
 
 
 
-export const calcularMonto = (modalidad, dias) => {
-    const hoy = obtenerFechaBogota();
-    const limitePreventa = new Date("2025-08-05T23:59:59");
+// export const calcularMonto = (modalidad, dias) => {
+//     const hoy = obtenerFechaBogota();
+//     const limitePreventa = new Date("2025-08-05T23:59:59");
 
+//     if (dias === "viernes_y_domingo") {
+//         return { moneda: "COP", valor: 0 };
+//     }
+
+//     if (modalidad === "presencial") {
+//         const valor = hoy <= limitePreventa ? 75000 : 90000;
+//         return { moneda: "COP", valor };
+//     }
+
+//     if (modalidad === "virtual") {
+//         return { moneda: "USD", valor: 15 };
+//     }
+
+//     return { moneda: "COP", valor: 0 };
+// };
+
+export const calcularMonto = (modalidad, dias) => {
     if (dias === "viernes_y_domingo") {
         return { moneda: "COP", valor: 0 };
-    }
-
-    if (modalidad === "presencial") {
-        const valor = hoy <= limitePreventa ? 75000 : 90000;
-        return { moneda: "COP", valor };
     }
 
     if (modalidad === "virtual") {
         return { moneda: "USD", valor: 15 };
     }
 
-    return { moneda: "COP", valor: 0 };
+    return { moneda: "COP", valor: 90000 };
 };
